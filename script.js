8d79792b6f32270b794b71623f6bcb3a8f2c516b
Chimelu <ezeoguinechimelu@gmail.com>
2023-10-02 21:05:11 +0100

    bankist

--- script.js
+++ script.js
@@ -216,19 +216,21 @@ const loadImg = function(entries , observer){
   const [entry] = entries
   // if(!entry.isIntersecting) return
   if (!entry.isIntersecting ) return
+    // replace src with data-src
   entry.target.src = entry.target.dataset.src
-  
+
   entry.target.addEventListener('load', function(){
     entry.target.classList.remove('lazy-img')
    })
   
-  // replace src with data-src
-  // entry.target.src = entry.target.dataset.src
+ 
+  observer.unobserve(entry.target)
 }
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: '+200px'
 
 })
 
